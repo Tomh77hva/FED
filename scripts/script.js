7ccbc8c7cb3389cc06ab892555eb354f0dab5711
Tom77H <tom.heine@hva.nl>
2026-01-25 20:01:25 +0000

    kyututf

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,70 @@
 // JavaScript Document
 console.log("hi");
+
+
+// const slidesContainer = document.getElementById("slides-container");
+// const slide = document.querySelector(".slide");
+// const prevButton = document.getElementById("slide-arrow-prev");
+// const nextButton = document.getElementById("slide-arrow-next");
+
+
+// nextButton.addEventListener("click", () => {
+// console.log("rechts");
+//   const slideWidth = slide.clientWidth;
+    
+//   slidesContainer.scrollLeft += slideWidth;
+
+// });
+
+// prevButton.addEventListener("click", () => {
+// console.log("links");
+//   const slideWidth = slide.clientWidth;
+
+//   slidesContainer.scrollLeft -= slideWidth;
+
+// });
+
+
+
+
+
+
+// const track = document.querySelector("trackid");
+// const slideWidth = track.querySelector("slide").offsetWidth;
+
+// document.querySelector("next").addEventListener("click", () => {
+//     console.log("rechts");
+//   track.scrollBy({ left: slideWidth, behavior: "smooth" });
+// });
+
+// document.querySelector("prev").addEventListener("click", () => {
+//     console.log("links");
+//   track.scrollBy({ left: -slideWidth, behavior: "smooth" });
+// });
+
+
+let currentIndex = 0;
+const images = document.querySelectorAll('.carouselimg');
+const totalImages = images.length;
+const carouselImages = document.getElementById('carouselImages');
+const indicators = document.querySelectorAll('.indicator');
+ 
+function updateCarousel() {
+    const offset = -currentIndex * 100;
+    carouselImages.style.transform = `translateX(${offset}%)`;
+               
+    indicators.forEach((indicator, index) => {
+    indicator.classList.toggle('active', index === currentIndex);
+    });}
+ 
+document.getElementById('nextbutton').addEventListener('click', () => {
+    console.log("rechts");
+    currentIndex = (currentIndex + 1 ) % totalImages;
+    updateCarousel();
+});
+ 
+document.getElementById('prevbutton').addEventListener('click', () => {
+    console.log("links");
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateCarousel();
+});
